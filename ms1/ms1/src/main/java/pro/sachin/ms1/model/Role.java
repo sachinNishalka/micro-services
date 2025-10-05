27f1a4d8c05cef5e90a5acead22628295aa13f29
sachinNishalka <72740598+sachinNishalka@users.noreply.github.com>
2025-10-05 09:30:18 +0530

    updated code

---
+++ ms1/ms1/src/main/java/pro/sachin/ms1/model/Role.java
@@ -0,0 +1,25 @@
+package pro.sachin.ms1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Set;
+
+@Data
+@Entity
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
---
+++ ms1/ms1/src/main/java/pro/sachin/ms1/model/User.java
@@ -0,0 +1,21 @@
+package pro.sachin.ms1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+}
